e276d0e36a83ed26d6398135eddcc52f9718a54e
luteh <luthfanmaftuh@gmail.com>
2019-03-17 22:49:57 +0700

    Refactor some files name and add handler if screen orientation changed

--- mangareader/src/main/java/com/luteh/mangareader/ui/discover/DiscoverFragment.kt
+++ mangareader/src/main/java/com/luteh/mangareader/ui/discover/DiscoverFragment.kt
@@ -4,6 +4,7 @@ package com.luteh.mangareader.ui.discover
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.app.Fragment
+import android.content.res.Configuration
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,11 +15,10 @@ import com.luteh.mangareader.R
 import com.luteh.mangareader.common.base.BaseFragment
 import com.luteh.mangareader.di.component.DaggerFragmentComponent
 import com.luteh.mangareader.di.module.FragmentModule
-import com.luteh.mangareader.model.Comic
 import com.luteh.mangareader.model.Manga
 import com.luteh.mangareader.ui.adapter.MangaAdapter
 import com.luteh.mangareader.ui.main.DiscoverContract
-import kotlinx.android.synthetic.main.fragment_discover.*
+import kotlinx.android.synthetic.main.discover_fragment.*
 import javax.inject.Inject
 
 /**
@@ -31,17 +31,17 @@ class DiscoverFragment : BaseFragment(), DiscoverContract.View {
     lateinit var presenter: DiscoverContract.Presenter
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discover, container, false)
+        return inflater.inflate(R.layout.discover_fragment, container, false)
     }
 
     override fun injectDependency() {
         val discoverComponent = DaggerFragmentComponent.builder()
-                .fragmentModule(FragmentModule())
-                .build()
+            .fragmentModule(FragmentModule())
+            .build()
         discoverComponent.inject(this)
     }
 
@@ -51,7 +51,7 @@ class DiscoverFragment : BaseFragment(), DiscoverContract.View {
     }
 
     private fun initView() {
-        initRecycler()
+        initRecycler(3)
 
         initSwipeRefresh()
 
@@ -59,14 +59,15 @@ class DiscoverFragment : BaseFragment(), DiscoverContract.View {
 
 //        presenter.loadMangaListData()
         pb_discover.visibility = View.VISIBLE
+        rv_discover.visibility = View.GONE
     }
 
     @SuppressLint("ResourceAsColor")
     private fun initSwipeRefresh() {
         //        First, load banner and comic
         swipe_to_refresh.setColorSchemeColors(
-                R.color.colorPrimary,
-                R.color.colorPrimaryDark
+            R.color.colorPrimary,
+            R.color.colorPrimaryDark
         )
         swipe_to_refresh.setOnRefreshListener {
             //            presenter.loadBannerData()
@@ -80,11 +81,10 @@ class DiscoverFragment : BaseFragment(), DiscoverContract.View {
         }
     }
 
-    private fun initRecycler() {
+    private fun initRecycler(spanCount: Int) {
         rv_discover.apply {
-            visibility = View.GONE
             setHasFixedSize(true)
-            layoutManager = GridLayoutManager(context, 3)
+            layoutManager = GridLayoutManager(context, spanCount)
         }
     }
 
@@ -107,5 +107,15 @@ class DiscoverFragment : BaseFragment(), DiscoverContract.View {
         if (swipe_to_refresh.isRefreshing) {
             swipe_to_refresh.isRefreshing = false
         }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        // Handle items count if screen orientation changed
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
+            initRecycler(R.integer.grid_span_count_landscape)
+        else
+            initRecycler(R.integer.grid_span_count_portrait)
     }
 }
--- mangareader/src/main/java/com/luteh/mangareader/ui/home/HomeActivity.kt
+++ mangareader/src/main/java/com/luteh/mangareader/ui/home/HomeActivity.kt
@@ -5,7 +5,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.luteh.mangareader.R
 import com.luteh.mangareader.common.base.BaseActivity
 import com.luteh.mangareader.ui.discover.DiscoverFragment
-import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.home_activity.*
 import com.luteh.mangareader.common.utils.BottomNavigationBehavior
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 
@@ -14,7 +14,7 @@ class HomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.luteh.mangareader.R.layout.activity_home)
+        setContentView(com.luteh.mangareader.R.layout.home_activity)
         setSupportActionBar(toolbar_home)
     }
 
